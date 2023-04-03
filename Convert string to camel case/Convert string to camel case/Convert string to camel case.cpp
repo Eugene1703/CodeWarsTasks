b6ad8146d9d480c2d26daa1414d0855a551a950a
Eugene1703 <91949945+Eugene1703@users.noreply.github.com>
2023-04-03 13:32:18 +0300

    TaskAdd
    AddTask

---
+++ Convert string to camel case/Convert string to camel case/Convert string to camel case.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+
+std::string to_camel_case(std::string text)
+{
+    std::string answer;
+    bool toupperNext = false;
+
+    for (char c : text)
+    {
+        if (c == '-' || c == '_')
+        {
+            toupperNext = true;
+        }
+        else if (toupperNext)
+        {
+            answer += toupper(c);
+            toupperNext = false;
+        }
+        else
+        {
+            answer += c;
+        }
+    }
+
+    return answer;
+}
+
+int main() {
+    std::cout << to_camel_case("Semioshko-Eugene_11") << std::endl;  // outputs "SemioshkoEugene11"
+    std::cout << to_camel_case("eugene_semioshko") << std::endl; // outputs "eugeneSemioshko"
+    std::cout << to_camel_case("Semioshko_test") << std::endl;        // outputs "SemioshkoTest"
+    return 0;
+}
+
+
+
+
