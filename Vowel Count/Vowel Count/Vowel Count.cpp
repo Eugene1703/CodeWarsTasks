5a1bd84ad82533772560e84d0709d06294b76183
Eugene1703 <91949945+Eugene1703@users.noreply.github.com>
2023-04-04 14:55:42 +0300

    VowelCount
    VowelCountTaskAdd

---
+++ Vowel Count/Vowel Count/Vowel Count.cpp
@@ -0,0 +1,21 @@
+﻿
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int getCount(const string& inputStr) {
+    int num_vowels = 0;
+    for (char c : inputStr)
+    {
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+            num_vowels++;
+    }
+    return num_vowels;
+}
+int main()
+{
+    cout << getCount("eugene") << endl; //output "4"
+    cout << getCount("aboba") << endl; //output "3"
+    cout << getCount("qprtsvf") << endl; //output "0"
+}
