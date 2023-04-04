b4cbbbfbac600f1a376aafbfe2c466e3efd512c8
Eugene1703 <91949945+Eugene1703@users.noreply.github.com>
2023-04-04 23:02:32 +0300

    Shortest Word
    Shortest Word Add

---
+++ Shortest Word/Shortest Word/Shortest Word.cpp
@@ -0,0 +1,34 @@
+﻿
+#include <iostream>
+#include <string>
+#include<vector>
+#include<algorithm>
+using namespace std;
+int find_short(string str)
+{
+    vector<int> vect;
+    int nmbr = 0;
+    for (char c : str) 
+    {
+        if (c != ' ')
+        {
+            nmbr++;
+        }
+        else 
+        { 
+            vect.push_back(nmbr); 
+            nmbr = 0;
+        }
+    }
+    vect.push_back(nmbr);
+    sort(vect.begin(), vect.end());
+    for (int i = 0; i < vect.size(); i++) 
+    {
+        cout << vect[i] << endl;
+    }
+    return vect[0];
+}
+int main()
+{
+    cout << find_short("Let's travel abroad shall we"); // output "2"
+}
